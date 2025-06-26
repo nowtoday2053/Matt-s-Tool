2962121a3cb1bbd03a6d57dad7ef37a6ee95d27b
nowtoday2053 <nowtoday2053@gmail.com>
2025-06-25 22:45:11 -0500

    added new ones

--- app.py
+++ app.py
@@ -443,4 +443,5 @@ def process_file(job_id, file_path):
 
 if __name__ == '__main__':
     print("Starting Phone Validator Tool...")
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
