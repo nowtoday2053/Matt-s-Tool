bccc04d2615de40f28b71001a3ad292708a100eb
nowtoday2053 <nowtoday2053@gmail.com>
2025-06-27 10:46:20 -0500

    ajewd

--- app.py
+++ app.py
@@ -161,4 +161,5 @@ def download_file(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host="0.0.0.0", port=8080)
+ 
